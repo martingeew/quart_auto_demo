4e623ad5d8953d11e68b2432171fc4bcc5138763
martingeew <67925955+martingeew@users.noreply.github.com>
2025-03-27 21:51:39 +0100

    add .gitkeep files and update data processing to save raw and processed stock data

--- extract_data.py
+++ extract_data.py
@@ -7,9 +7,16 @@ data = ffn.get("nvda,msft,googl,amzn,aapl,meta", start="2023-01-01")
 # Step 2: Rebase data
 data_rebase = data.rebase()
 
-# Step 3: Save pandas dataframe as csv and the plot as a PNG
+# Step 3: Save raw data as csv
 timestamp = datetime.now().strftime("%Y-%m-%d")
 csv_filename = f"stock_data_{timestamp}.csv"
-data_rebase.to_csv(f"data/stock_data_{timestamp}.csv")
+data.to_csv(f"data/raw/stock_data_{timestamp}.csv")
 
-print(f"Data saved to {csv_filename}")
+print(f"Raw data saved to {csv_filename}")
+
+# Step 4: Save processed data as csv
+timestamp = datetime.now().strftime("%Y-%m-%d")
+processed_csv_filename = f"stock_data_{timestamp}.csv"
+data_rebase.to_csv(f"data/processed/stock_data_rebased_{timestamp}.csv")
+
+print(f"Processed data saved to {processed_csv_filename}")
